f8d070710f903c511e480c3ab4e517680f6dd934
chark1es <charles15851@gmail.com>
2024-03-15 20:58:10 -0700

    incorporate frameworks into astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind(),
+        preact({
+            include: ["**/preact/*"],
+        }),
+        react({
+            include: ["**/react/*"],
+        }),
+    ],
 });
