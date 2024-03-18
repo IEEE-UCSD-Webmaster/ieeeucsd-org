10b78f86a3da33479caeae26c21f9ccef6eb5d5a
chark1es <charles15851@gmail.com>
2024-03-17 22:08:07 -0700

    test netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
+        mdx(),
         tailwind(),
         preact({
             include: ["**/preact/*"],
