3eda65a188d2fac2a05d0f9c7961eb06503a8087
chark1es <charles15851@gmail.com>
2024-05-14 12:19:21 -0700

    fix icons not being rendered

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,13 @@ import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        mdx(),
-        tailwind(),
-        preact({
-            include: ["**/preact/*"],
-        }),
-        react({
-            include: ["**/react/*"],
-        }),
-    ],
+  integrations: [mdx(), tailwind(), preact({
+    include: ["**/preact/*"]
+  }), react({
+    include: ["**/react/*"]
+  }), icon()]
 });
