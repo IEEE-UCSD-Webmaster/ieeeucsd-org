0aa0d3b4f082916ee116bd43c8e5de0373fa8043
chark1es <charles15851@gmail.com>
2024-09-14 02:28:21 -0700

    add nodejs to build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,25 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), preact({
-    include: ["**/preact/*"]
-  }), react({
-    include: ["**/react/*"]
-  }), icon()]
+    integrations: [
+        mdx(),
+        tailwind(),
+        preact({
+            include: ["**/preact/*"],
+        }),
+        react({
+            include: ["**/react/*"],
+        }),
+        icon(),
+    ],
+    output: "server",
+    adapter: node({
+        mode: "standalone",
+    }),
 });
